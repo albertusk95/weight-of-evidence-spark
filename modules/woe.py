9dd46fd58e2fe90a4dfbcafad5c94ddd568855da
Albertus Kelvin <albertus.kelvin@gdplabs.id>
2020-04-06 15:58:05 +0700

    fit done

--- modules/woe.py
+++ modules/woe.py
@@ -1,3 +1,5 @@
+import math
+
 from pyspark.sql import DataFrame
 from pyspark.sql import functions as F
 
@@ -8,26 +10,49 @@ class WOE_IV(object):
         self.cols_to_woe = cols_to_woe
         self.label_column = label_column
         self.good_label = good_label
+        self.fit_data = {}
 
-    def compute_woe(self):
+    def fit(self):
         for col_to_woe in self.cols_to_woe:
+            total_good = self.compute_total_amount_of_good()
+            total_bad = self.compute_total_amount_of_bad()
+            
             woe_df = self.df.select(col_to_woe)
             categories = woe_df.distinct().collect()
             for category_row in categories:
                 category = category_row[col_to_woe]
+                good_dist = self.compute_good_amount(col_to_woe, category) / total_good
+                bad_dist = self.compute_bad_amount(col_to_woe, category) / total_bad
 
-                good_dist = self.compute_good_dist(col_to_woe, category)
-                bad_dist = self.compute_bad_dist(col_to_woe, category)
+                good_dist = good_dist if good_dist is not None else 0.0
+                bad_dist = bad_dist if bad_dist is not None else 0.0
 
-                
+                woe_info = {
+                    'category': category,
+                    'woe': math.log(good_dist / bad_dist)
+                }
 
-    def compute_good_dist(self, col_to_woe: str, category: str):
+                self.fit_data[col_to_woe] = woe_info
+
+    def transform(self):
+        for col_to_woe, woe_info in self.fit_data.items():
+            self.df = self.df.withColumn(col_to_woe + '_woe', )
+
+
+
+    def compute_total_amount_of_good(self):
+        return self.df.select(self.label_column).filter(F.col(self.label_column) == self.good_label).count()
+
+    def compute_total_amount_of_bad(self):
+        return self.df.select(self.label_column).filter(F.col(self.label_column) != self.good_label).count()
+
+    def compute_good_amount(self, col_to_woe: str, category: str):
         return self.df.select(col_to_woe, self.label_column)\
                       .filter(
                             (F.col(col_to_woe) == category) & (F.col(self.label_column) == self.good_label)
                       ).count()
 
-    def compute_bad_dist(self, col_to_woe: str, category: str):
+    def compute_bad_amount(self, col_to_woe: str, category: str):
         return self.df.select(col_to_woe, self.label_column)\
                       .filter(
                             (F.col(col_to_woe) == category) & (F.col(self.label_column) != self.good_label)
