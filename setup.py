92b4befe6c782897ccbc54bbecd3982618fe1e8e
Albertus Kelvin <albertus.kelvin@gdplabs.id>
2020-04-06 18:21:04 +0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='weight-of-evidence-information-value',
+      version='0.1',
+      description='Weight of Evidence & Information Value with PySpark',
+      url='https://github.com/albertusk95/weight-of-evidence-spark',
+      author='Albertus Kelvin',
+      license='Copyright (C) Albertus Kelvin - All Rights Reserved',
+      python_requires=">=3.7.0",
+      packages=find_packages())
